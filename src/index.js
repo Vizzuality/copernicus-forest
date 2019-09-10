2c1144ad42090b189a0404e01c46851728e84852
Daniel Fernandez Rico <deferrico@gmail.com>
2019-09-10 17:14:00 +0200

    Added a basic router to the page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './AppRouter';
 import * as serviceWorker from './serviceWorker';
 
 import './styles/index.scss';
