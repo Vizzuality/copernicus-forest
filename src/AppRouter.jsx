1329c0291389fdac37da82aea85cb7b0f9113899
Daniel Fernández Rico <deferrico@gmail.com>
2019-09-13 12:12:43 +0200

    Added lazy loading to router component

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,26 +1,36 @@
-import React from 'react';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
-
-import Header from 'components/header';
-import HomePage from 'pages/home';
-import SpeciesPage from 'pages/species';
-import DistributionPage from 'pages/distribution';
-import BioclimaticPage from 'pages/bioclimatic';
-import CountryPage from 'pages/country';
+import React, { lazy, Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 import './App.scss';
+
+const Header = lazy(() => import('components/header'));
+
+const HomePage = lazy(() => import('pages/home'));
+
+const SpeciesPage = lazy(() => import('pages/species'));
+
+const DistributionPage = lazy(() => import('pages/distribution'));
+
+const BioclimaticPage = lazy(() => import('pages/bioclimatic'));
+
+const CountryPage = lazy(() => import('pages/country'));
+
+const Placeholder = () => <div className="c-header" />;
 
 function AppRouter() {
   return (
     <Router>
       <div className="c-app">
-        <Header />
-
-        <Route path="/" exact component={HomePage} />
-        <Route path="/:iso/species/:id" exact component={SpeciesPage} />
-        <Route path="/:iso/species/:id/distribution" exact component={DistributionPage} />
-        <Route path="/:iso/bioclimatic/:id" exact component={BioclimaticPage} />
-        <Route path="/:iso" exact component={CountryPage} />
+        <Suspense fallback={<Placeholder />}>
+          <Header />
+          <Switch>
+            <Route path="/" exact component={HomePage} />
+            <Route path="/:iso/species/:id" component={SpeciesPage} />
+            <Route path="/:iso/species/:id/distribution" component={DistributionPage} />
+            <Route path="/:iso/bioclimatic/:id" component={BioclimaticPage} />
+            <Route path="/:iso" component={CountryPage} />
+          </Switch>
+        </Suspense>
       </div>
     </Router>
   );
