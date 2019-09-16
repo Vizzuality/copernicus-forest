7b2d1a1b9f080ce25e3bf0c14667928ccd2554c1
Daniel Fernández Rico <deferrico@gmail.com>
2019-09-16 15:27:15 +0200

    Fixed Router imports

--- src/AppRouter.jsx
+++ src/AppRouter.jsx
@@ -1,28 +1,14 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-
 import Icons from 'components/icons';
-import Header from 'components/header';
-import HomePage from 'pages/home';
-import SpeciesPage from 'pages/species';
-import DistributionPage from 'pages/distribution';
-import BioclimaticPage from 'pages/bioclimatic';
-import CountryPage from 'pages/country';
-
 import './App.scss';
 
 const Header = lazy(() => import('components/header'));
-
 const HomePage = lazy(() => import('pages/home'));
-
 const SpeciesPage = lazy(() => import('pages/species'));
-
 const DistributionPage = lazy(() => import('pages/distribution'));
-
 const BioclimaticPage = lazy(() => import('pages/bioclimatic'));
-
 const CountryPage = lazy(() => import('pages/country'));
-
 const Placeholder = () => <div className="c-header" />;
 
 function AppRouter() {
