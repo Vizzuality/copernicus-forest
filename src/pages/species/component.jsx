a3b4792a3a48018797f63477e31be08af10b9202
Daniel Fernández Rico <deferrico@gmail.com>
2019-10-02 13:24:43 +0200

    Added urql with useQuery hooks in species page

--- src/pages/species/component.jsx
+++ src/pages/species/component.jsx
@@ -1,14 +1,36 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { useQuery } from 'urql';
 import './styles.scss';
 
 function SpeciesPage({ match }) {
   const { iso, id } = (match && match.params) || {};
+  const [result] = useQuery({
+    query: `{ countries { iso, name } }`
+  });
+
+  const { fetching, data, error } = result;
+
+  let countries = [];
+  let activeCountry;
+
+  if (data) {
+    countries = data.countries;
+    activeCountry = data.countries.find(c => c.iso === iso);
+  }
+
   return (
     <div className="c-species">
-      <span>
-        Species in {iso} with id {id}
-      </span>
+      {fetching && <p>Loading...</p>}
+      {error && <p>Error retrieving the data</p>}
+      {!fetching && !error && (
+        <div className="content">
+          <p>We have data from {countries.map(c => c.name).join(' and ')}.</p>
+          <p>
+            Species in {activeCountry ? activeCountry.name : iso} with id {id}
+          </p>
+        </div>
+      )}
     </div>
   );
 }
