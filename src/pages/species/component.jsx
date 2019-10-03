e11d3c1432b81700cbfc8bbef0322c883672294d
Daniel Fernandez Rico <deferrico@gmail.com>
2019-10-03 20:23:10 +0200

    Composed query with multiple effects to set multiple values

--- src/pages/species/component.jsx
+++ src/pages/species/component.jsx
@@ -1,23 +1,61 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { useQuery } from 'urql';
 import './styles.scss';
 
 function SpeciesPage({ match }) {
   const { iso, id } = (match && match.params) || {};
+  const wikiURL = 'https://en.wikipedia.org/api/rest_v1/page/summary/';
+  const [wikiInfo, setInfo] = useState('');
+  const [activeCountry, setCountry] = useState(null);
+  const [species, setSpecies] = useState([]);
+  const [activeSpecie, setSpecie] = useState(null);
+
   const [result] = useQuery({
-    query: `{ countries { iso, name } }`
+    query: `
+    {
+      countrySpecieDistributions(where:{
+        country: {
+          iso: "SWE"
+        }
+      }) {
+        country {
+          iso,
+          name
+        },
+        specie {
+          name,
+          wikipediaSlug,
+          id
+        }
+      }
+    }
+    `
   });
 
   const { fetching, data, error } = result;
 
-  let countries = [];
-  let activeCountry;
+  useEffect(() => {
+    if (data) {
+      const distrib = data.countrySpecieDistributions;
+      setCountry(distrib && distrib[0] && distrib[0].country);
+      setSpecies(distrib.map(d => d.specie));
+    }
+  }, [data, setCountry, setSpecies]);
 
-  if (data) {
-    countries = data.countries;
-    activeCountry = data.countries.find(c => c.iso === iso);
-  }
+  useEffect(() => {
+    if (species && species.length) setSpecie(species.find(sp => sp.id === id) || species[0]); // TODO: modify with right IDs
+  }, [id, species, setSpecie]);
+
+  useEffect(() => {
+    if (activeSpecie) {
+      fetch(`${wikiURL}${activeSpecie && activeSpecie.wikipediaSlug}`)
+        .then(r => r.json())
+        .then(r => {
+          setInfo(r.extract);
+        });
+    }
+  }, [activeSpecie, setInfo]);
 
   return (
     <div className="c-species">
@@ -25,10 +63,14 @@ function SpeciesPage({ match }) {
       {error && <p>Error retrieving the data</p>}
       {!fetching && !error && (
         <div className="content">
-          <p>We have data from {countries.map(c => c.name).join(' and ')}.</p>
           <p>
             Species in {activeCountry ? activeCountry.name : iso} with id {id}
           </p>
+          <br />
+          <p>List of species:</p>
+          {species && species.map(s => <p key={s.id}>{s.name}</p>)}
+          <br />
+          <p>{wikiInfo}</p>
         </div>
       )}
     </div>
