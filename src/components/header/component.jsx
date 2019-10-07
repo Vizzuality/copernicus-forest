0af2fdaed700df796ab90dfee8fdcd2c9b26de53
Daniel Fernández Rico <deferrico@gmail.com>
2019-10-07 16:28:19 +0200

    Added menu to header

--- src/components/header/component.jsx
+++ src/components/header/component.jsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './styles.scss';
+import Menu from 'components/menu';
+import Icon from 'components/icon';
 
 function Header() {
+  const [menuOpen, showMenu] = useState(false);
   return (
     <div className="c-header">
       {/* logo placeholder */}
       <Link to="/">
         <span>GLOBAL FOREST CLIMATE SERVICES</span>
       </Link>
+      <button className="menu-button" onClick={() => showMenu(!menuOpen)}>
+        <Icon name="icon-menu" className="menu-icon" />
+        About
+      </button>
+      <Menu closeMenu={() => showMenu(false)} active={menuOpen} />
     </div>
   );
 }
