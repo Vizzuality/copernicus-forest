c4423136a35da1b8d4656bedb285ef8c34f91299
Kamil Filip <kamil.filip94@gmail.com>
2019-10-29 12:15:27 +0000

    Implement timestep component from vizzuality-components

--- src/components/map/controls/timeline/component.jsx
+++ src/components/map/controls/timeline/component.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import cx from 'classnames';
+import { LegendItemTimeStep } from 'vizzuality-components';
 
 import styles from './styles.module.scss';
 
@@ -20,6 +21,45 @@ const TimelineComponent = ({ activeTab = 'rcp85', setActiveTab, className }) => 
     ]
   };
 
+
+  const timelineParams = {
+    canPlay: true,
+    dateFormat: "YYYY",
+    interval: "years",
+    minDate: "2020-01-01",
+    maxDate: "2090-12-31",
+    startDate: "2020-01-01",
+    endDate: "2090-12-31",
+    trimEndDate: "2016-09-14",
+    speed: 250,
+    step: 1,
+    marks: [],
+    handleStyle: {
+      backgroundColor: '#5C5C5C',
+      borderRadius: '0',
+      // boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.29)',
+      border: '2px solid white',
+      height: '12px',
+      transform: 'translateY(2px)',
+      width: '6px',
+      zIndex: 2
+    },
+    trackStyle: {
+      backgroundColor: '#DF5127',
+      height: '6px'
+    },
+    railStyle: {
+      backgroundColor: '#5C5C5C',
+      borderRadius: '0',
+      height: '6px'
+    },
+    range: false,
+    trackColors: {},
+    className: 'xd',
+    playButton: <div>dups</div>
+  };
+  
+
   return (
     <div className={cx(styles.container, className)}>
       {data && (
@@ -36,6 +76,13 @@ const TimelineComponent = ({ activeTab = 'rcp85', setActiveTab, className }) => 
           ))}
         </div>
       )}
+      <LegendItemTimeStep
+        playButton={<div>dups</div>}
+        activeLayer={{ id: 'id', timelineParams }}
+        handleOnChange={date => console.log('handleOnChange')}
+        handleChange={date => console.log('handleChange')}
+        // handleOnAfterChange={date => console.log('handleOnAfterChange')}
+      />
     </div>
   );
 };
@@ -47,3 +94,20 @@ TimelineComponent.propTypes = {
 };
 
 export default TimelineComponent;
+
+          // {...this.props}
+          // activeLayer={{
+          //   ...activeLayer,
+          //   timelineParams: {
+          //     ...activeLayer.timelineParams,
+          //     marks: this.props.marks,
+          //     handleStyle: {
+          //       backgroundColor: 'white',
+          //       borderRadius: '2px',
+          //       boxShadow: '0 1px 2px 0 rgba(0, 0, 0, 0.29)',
+          //       border: '0px',
+          //       zIndex: 2
+          //     }
+          //   }
+          // }}
+        // />
