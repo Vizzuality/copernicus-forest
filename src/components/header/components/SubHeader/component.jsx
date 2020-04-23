135e31767ed3e06147e237b952ceb263352b53e3
dancer2090 <ivaneichyk.ivan@gmail.com>
2020-04-24 01:04:18 +0300

    navigation component - SubMenu - routes + module.scss

--- src/components/header/components/SubHeader/component.jsx
+++ src/components/header/components/SubHeader/component.jsx
@@ -1,77 +1,84 @@
 import React from 'react';
-import Icon from 'components/icon';
+import { useRouteMatch, useLocation, Link } from 'react-router-dom';
 import cx from 'classnames';
-
-import { Link } from 'react-router-dom';
-
-import './styles.scss';
-
-
-const subMenu = [
-  {
-    title: 'Sweeden',
-    sections: [
-      {
-        title: 'Canada'
-      },
-      {
-        title: 'Indonesia'
-      },
-      {
-        title: 'Sweeden'
-      },
-      {
-        title: 'Peru'
-      },
-      {
-        title: 'Tanzania'
-      }
-    ]
-  },
-  {
-    title: 'Species distribution',
-    sections: [
-      {
-        title: 'Map distribution'
-      },
-      {
-        title: 'Species summary'
-      }
-    ]
-  },
-  {
-    title: 'Bioclimatic variables'
-  }
-];
+import Icon from 'components/icon';
+import { COUNTRIES } from 'constants.js';
+import styles from './styles.module.scss';
 
 const SubHeader = () => {
+  const match = useRouteMatch('/:iso/:type/:id?');
+  const { iso, type, id } = (match && match.params) || {};
+  const { pathname } = useLocation();
+
+  const urls = {
+    species: type === 'species' ? '#' : `/${iso}/species/${id}`,
+    distribution: type === 'distribution' ? '#' : `/${iso}/distribution/${id}`,
+    bioclimatic: type === 'bioclimatic' ? '#' : `/${iso}/bioclimatic/${id}`
+  };
+
+  const speciesTabsData = [
+    {
+      name: 'Species summary',
+      path: urls.species,
+      active: pathname.includes('/species'),
+      page: 'species'
+    },
+    {
+      name: 'Map distribution',
+      path: urls.distribution,
+      active: pathname.includes('/distribution'),
+      page: 'distribution'
+    }
+  ];
+
+  const subMenu = [
+    {
+      name: COUNTRIES.find(c => c.iso === iso).name,
+      sections: COUNTRIES.map(c => {
+        c.path = `/${c.iso}/${type}/${id}`;
+        c.active = pathname.includes(`/${c.iso}`);
+        return c;
+      })
+    },
+    {
+      name: 'Species distribution',
+      active: pathname.includes('/species') || pathname.includes('/distribution'),
+      sections: speciesTabsData
+    },
+    {
+      name: 'Bioclimatic variables',
+      active: pathname.includes('/bioclimatic'),
+      path: urls.bioclimatic
+    }
+  ];
+
   return (
-    <>
-      <div className="c-subheader">
-        <div className="container">
-          <Link to="/" className="logo">
-            <img src="/logos/logo.svg" alt="logo" />
-          </Link>
-          <ul className="sub-menu">
-            {subMenu.map(s => (
-              <li>
-                {s.title}
-                {s.sections && s.sections.length > 0 && (
-                  <>
-                    <Icon name="icon-triangle" />
-                    <ul>
-                      {s.sections.map(sec => (
-                        <li>{sec.title}</li>
-                      ))}
-                    </ul>
-                  </>
-                )}
-              </li>
-            ))}
-          </ul>
-        </div>
+    <div className={styles.cSubheader}>
+      <div className={styles.container}>
+        <Link to="/" className={styles.logo}>
+          <img src="/logos/logo.svg" alt="logo" />
+        </Link>
+        <ul className={styles.subMenu}>
+          {subMenu.map(s => (
+            <li className={cx(styles.menuItem, { [styles.active]: s.active })}>
+              {s.path ? <Link to={s.path}>{s.name}</Link> : <>{s.name}</>}
+              {s.sections && s.sections.length > 0 && (
+                <>
+                  <Icon name="icon-triangle" />
+                  <ul>
+                    {s.sections.map(sec => (
+                      <li className={sec.active ? styles.active : ''}>
+                        <Link to={sec.path}>{sec.name}</Link>
+                      </li>
+                    ))}
+                  </ul>
+                </>
+              )}
+            </li>
+          ))}
+        </ul>
       </div>
-    </>
+    </div>
   );
 };
 
