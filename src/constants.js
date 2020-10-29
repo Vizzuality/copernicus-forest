ded04f2c8e5fe8b65ac595f12a98d7354f532ba3
Daniel Fernandez Rico <deferrico@gmail.com>
2020-10-29 11:08:57 +0100

    Fixed image tag in modal

--- src/constants.js
+++ src/constants.js
@@ -137,112 +137,124 @@ export const DISTRIBUTIONS = {
 export const MODAL_INFO_DATA = {
   currentDistribution: {
     title: 'Current Distribution',
-    text: (<>
-      <p>The current observed distribution shows the points from GBIF where the species has been observed in the last 20 years. The modelled distribution is the committee average of the 180 calibrated models.</p>
-      <p>Access the data files at <a href="https://doi.org/10.5281/zenodo.4055178"><img src="https://zenodo.org/badge/DOI/10.5281/zenodo.4055178.svg" alt="DOI"></a></p>
-      </>)},
+    text: (
+      <>
+        <p>
+          The current observed distribution shows the points from GBIF where the species has been
+          observed in the last 20 years. The modelled distribution is the committee average of the
+          180 calibrated models.
+        </p>
+        <p>
+          Access the data files at{' '}
+          <a href="https://doi.org/10.5281/zenodo.4055178">
+            <img src="https://zenodo.org/badge/DOI/10.5281/zenodo.4055178.svg" alt="DOI" />
+          </a>
+        </p>
+      </>
+    )
+  },
   futureDistribution: {
-        title: 'Future Distribution',
+    title: 'Future Distribution',
     text:
       'The future modelled distribution is the projection of the model calibrated with the current observations to the future climatic conditions.'
   },
   committeeAverage: {
-        title: 'Committee Average',
+    title: 'Committee Average',
     text:
       'The Committee Average depicts the result of all the calibrated models voting for an absence or a presence and then averaged. Values close to 0 represent an agreement of the models on an absence. Values close to 1000 represent an agreement of the models on a presence. Values around 500 inform on a disagreement on the prediction of the models.'
   },
   biovar01: {
-        title: 'Annual Mean Temperature',
+    title: 'Annual Mean Temperature',
     text:
       'The monthly maximum and minimum temperature for each month have been extracted and then the average has been obtained for each month. The monthly averages have been averaged to obtain the annual mean temperature. The annual mean temperatures have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country'
   },
   biovar02: {
-        title: 'Mean Diurnal Range (Mean of monthly (max temp - min temp))',
+    title: 'Mean Diurnal Range (Mean of monthly (max temp - min temp))',
     text:
       'The monthly maximum and minimum temperature for each month have been extracted. The minimum has been subtracted to the maximum. The monthly values have been averaged to obtain the annual mean diurnal range. The annual mean diurnal range has been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar03: {
-        title: 'Isothermality (BIO2/BIO7) (×100)',
+    title: 'Isothermality (BIO2/BIO7) (×100)',
     text:
       'The Mean diurnal range (BIO2) has been divided by the Temperature Annual Range (BIO7). The information shown on the map is an average across the period. On the graph, the information shown is averaged across the country.'
   },
   biovar04: {
-        title: 'Temperature Seasonality (standard deviation ×100)',
+    title: 'Temperature Seasonality (standard deviation ×100)',
     text:
       'For each year of a period, the monthly maximum and minimum temperature for each month have been extracted. The standard deviation has been calculated and multiplied by 100. The annual temperature seasonality has been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar05: {
-        title: 'Max Temperature of Warmest Month',
+    title: 'Max Temperature of Warmest Month',
     text:
       'For each year of a period, the maximum monthly temperature has been extracted. The maximum temperature of the warmest month of each year of a period has been averaged, which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar06: {
-        title: 'Min Temperature of Coldest Month',
+    title: 'Min Temperature of Coldest Month',
     text:
       'For each year of a period, the minimum monthly temperature has been extracted. The minimum temperature of the coldest month of each year of a period has been averaged, which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar07: {
-        title: 'Temperature Annual Range (BIO5-BIO6)',
+    title: 'Temperature Annual Range (BIO5-BIO6)',
     text:
       'The minimum temperature of the coldest month has been subtracted to the maximum temperature of the warmest month. The information shown on the map is an average across the period. On the graph, the information shown is averaged across the country.'
   },
   biovar08: {
-        title: 'Mean Temperature of Wettest Quarter',
+    title: 'Mean Temperature of Wettest Quarter',
     text:
       'For each year, the total precipitation of each month has been extracted and the three consecutive months with the highest precipitation have been identified. From those three months the minimum and maximum temperature have been extracted and averaged. The mean temperatures of each year have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar09: {
-        title: 'Mean Temperature of Driest Quarter',
+    title: 'Mean Temperature of Driest Quarter',
     text:
       'For each year, the total precipitation of each month has been extracted and the three consecutive months with the lowest precipitation have been identified. From those three months the minimum and maximum temperature have been extracted and averaged. The mean temperatures of each year have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar10: {
-        title: 'Mean Temperature of Warmest Quarter',
+    title: 'Mean Temperature of Warmest Quarter',
     text:
       'For each year, the maximum temperature of each month has been extracted and the three consecutive months with the highest temperature have been identified. From those three months the minimum and maximum temperature have been extracted and averaged. The mean temperatures of each year have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar11: {
-        title: 'Mean Temperature of Coldest Quarter',
+    title: 'Mean Temperature of Coldest Quarter',
     text:
       'For each year, the minimum temperature of each month has been extracted and the three consecutive months with the lowest temperature have been identified. From those three months the minimum and maximum temperature have been extracted and averaged. The mean temperatures of each year have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar12: {
-        title: 'Annual Precipitation',
+    title: 'Annual Precipitation',
     text:
       'For each year, the total precipitation of each month has been extracted and summed across the year. The annual precipitations have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar13: {
-        title: 'Precipitation of Wettest Month',
+    title: 'Precipitation of Wettest Month',
     text:
       'For each year, the total precipitation of each month has been extracted and the three consecutive months with the highest precipitation have been identified. The yearly values have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar14: {
-        title: 'Precipitation of Driest Month',
+    title: 'Precipitation of Driest Month',
     text:
       'For each year, the total precipitation of each month has been extracted and the three consecutive months with the lowest precipitation have been identified. The yearly values have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar15: {
-        title: 'Precipitation Seasonality (Coefficient of Variation)',
+    title: 'Precipitation Seasonality (Coefficient of Variation)',
     text:
       'For each year the coefficient of variation between months has been calculated. The yearly values have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar16: {
-        title: 'Precipitation of Wettest Quarter.',
+    title: 'Precipitation of Wettest Quarter.',
     text:
       'For each year, the total precipitation of each month has been extracted and the three consecutive months with the highest precipitation have been identified. The sum of precipitation of those three months has been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar17: {
-        title: 'Precipitation of Driest Quarter',
+    title: 'Precipitation of Driest Quarter',
     text:
       'For each year, the total precipitation of each month has been extracted and the three consecutive months with the lowest precipitation have been identified. The sum of precipitation of those three months has been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar18: {
-        title: 'Precipitation of Warmest Quarter',
+    title: 'Precipitation of Warmest Quarter',
     text:
       'For each year, the maximum temperature of each month has been extracted and the three consecutive months with the highest temperature have been identified. From those three months the sum of precipitation has been extracted. The total precipitation of the warmest quarter of each year have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   },
   biovar19: {
-        title: 'Precipitation of Coldest Quarter',
+    title: 'Precipitation of Coldest Quarter',
     text:
       'For each year, the minimum temperature of each month has been extracted and the three consecutive months with the highest temperature have been identified. From those three months the sum of precipitation has been extracted. The total precipitation of the warmest quarter of each year have been averaged across a period which is the information shown on the map. On the graph, the information shown is averaged across the country.'
   }
