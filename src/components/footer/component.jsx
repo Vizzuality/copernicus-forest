b58b967e865c243e7d4c1d0f41a83da13c21d0ef
Daniel Fernandez Rico <deferrico@gmail.com>
2020-11-20 11:44:23 +0100

    Added footer attribution

--- src/components/footer/component.jsx
+++ src/components/footer/component.jsx
@@ -5,8 +5,25 @@ import styles from './styles.module.scss';
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      Created using data from the{' '}
-      <a href="https://www.copernicus.eu">Copernicus Climate Change Service</a>
+      <div className={styles.createdBy}>
+        Created using data from the{' '}
+        <a href="https://climate.copernicus.eu" target="_blank" rel="noopener noreferrer">
+          Copernicus Climate Change Service
+        </a>
+      </div>
+      <div className={styles.developedBy}>
+        Developed by:
+        <a
+          href="https://www.tecnalia.com/en/energy-and-environment/offer-to-companies/environment-and-sustainability/climate-change-adaptation.htm"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <img src="/logos/tecnalia.png" alt="Tecnalia" />
+        </a>
+        <a href="https://www.vizzuality.com/" target="_blank" rel="noopener noreferrer">
+          <img src="/logos/vizzuality.svg" alt="Vizzuality" />
+        </a>
+      </div>
     </footer>
   );
 };
